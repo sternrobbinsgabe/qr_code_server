87e47c10a80da7eb362557a91bd478460a2489e6
student <cs3516@cs3516machine.(none)>
2012-10-27 16:47:28 -0500

    added server and client

---
+++ qrcodes/main.cpp
@@ -0,0 +1,84 @@
+//THIS IS THE SERVER
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
+
+using namespace std;
+//recieve a string from the client and then print it
+
+//helper function to determine if we are using ipv4 or 6
+void* get_in_addr(struct sockaddr* sa){
+    //if ipv4, do this
+    if(sa->sa_family==AF_INET){
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    //if not ipv4, it must be 6... I hope
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main()
+{
+    //to do
+    //create a server on port 12345
+    //listen for incomming messages
+    //if it recieves one, then convert string to ALL CAPS and send it back to the client
+    //if it recieves an escape, close the connection
+
+    int sockfd, new_fd;
+    struct addrinfo hints, *serverinfo, *p;
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+    //struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+    char buf[128];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype=SOCK_STREAM;
+    hints.ai_flags=AI_PASSIVE;
+
+    getaddrinfo(NULL,"12345",&hints,&serverinfo);
+    sockfd=socket(serverinfo->ai_family,serverinfo->ai_socktype,serverinfo->ai_protocol);
+    bind(sockfd,serverinfo->ai_addr,serverinfo->ai_addrlen);
+
+    freeaddrinfo(serverinfo);
+
+    listen(sockfd,1);
+
+    while(1){
+        sin_size=sizeof their_addr;
+        new_fd=accept(sockfd,(struct sockaddr*)&their_addr,&sin_size);
+
+        //if we dont have a connection, keep waiting
+        if(new_fd==-1){
+            continue;
+        }
+        //cin to the buffer that we will send
+        while(1){
+
+        cout << "Type something" << endl;
+        cin>>buf;
+
+        inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr*)&their_addr),s,sizeof s);
+        cout<< "got a connection from: " << s << endl;
+
+        //close(sockfd);
+                    send(new_fd,buf,128,0);
+        }
+        //close(new_fd);
+    }
+
+    return 0;
+}
+
---
+++ qrcodes_client/main.cpp
@@ -0,0 +1,60 @@
+//THIS IS THE CLIENT
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+
+//for the checkpoint, the client will cin to a string and then the server will print that string
+
+//helper function to determine if we are using ipv4 or 6
+void* get_in_addr(struct sockaddr* sa){
+    //if ipv4, do this
+    if(sa->sa_family==AF_INET){
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    //if not ipv4, it must be 6... I hope
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main()
+{
+    //connect to server on 12345
+    //listen for messages from the server
+    //then print them yay
+    int sockfd, numbytes;
+    char buf[128];
+    struct addrinfo hints, *serverinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    memset(&hints,0,sizeof hints);
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype=SOCK_STREAM;
+
+    rv=getaddrinfo("cs3516machine","12345",&hints,&serverinfo);
+
+    sockfd=socket(serverinfo->ai_family,serverinfo->ai_socktype,serverinfo->ai_protocol);
+    connect(sockfd,serverinfo->ai_addr,serverinfo->ai_addrlen);
+
+    inet_ntop(serverinfo->ai_family,get_in_addr((struct sockaddr*)serverinfo->ai_addr),s,sizeof s);
+    cout << "connecting to: "<< s << endl;
+
+    while(1){
+        numbytes=recv(sockfd,buf,127,0);
+        if(numbytes>1){
+            buf[numbytes]='\0';
+            cout<< "Recieved " <<numbytes<< " bytes"<< endl;
+            cout << "Recieved "<< buf << endl;
+        }
+    }
+
+    return 0;
+}
