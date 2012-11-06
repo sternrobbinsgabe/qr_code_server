ef4867bce828e12afcd41c766dff2fb32641831f
student <cs3516@cs3516machine.(none)>
2012-11-06 18:08:01 -0500

    TODO: rate limit, rate request, max users, check for a disconnect, server time out

--- qrcodes/main.cpp
+++ qrcodes/main.cpp
@@ -4,7 +4,7 @@
 /**
  @author Gabriel Stern-Robbins, Lukasz Zawada
  @copyright 10/27/2012 WPI CS3516 B12
- @comment: This is the client for assignment 1.
+ @comment: This is the server for assignment 1.
 
 **/
 
@@ -20,7 +20,7 @@
 #include <signal.h>
 #include <errno.h>
 #include <stdlib.h>
-
+#include <time.h>
 #include <sys/stat.h>
 
 using namespace std;
@@ -36,60 +36,22 @@ void* get_in_addr(struct sockaddr* sa){
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-//returns -1 on failure, bytesSent on success
-int sendInChunks(int sockfd, char *buffer, int bytesLeft)
-{
-    int bytesSent = 0;        // how many bytes we've sent
-    int bytesSentSoFar;
-
-    while(bytesLeft > 0) {
-                            //socket, buffer iterator,  bytes left, flags
-        bytesSentSoFar = send(sockfd, buffer+bytesSent, bytesLeft, 0);
-        if (bytesSentSoFar == -1 ){
-            cout<<"ERROR: -1 bytes sent."<<endl;
-            break;
-        }
-        bytesSent += bytesSentSoFar;
-        bytesLeft -= bytesSentSoFar;
-    }
-
-    if (bytesSentSoFar == -1 ){
-        cout<<"ERROR: -1 bytes were sent."<<endl;
-        return -1;
-    }
-    else {
-        return bytesSent;
-    }
-}
-
 //function that gets the url from a qr file
 char* qr2url(char* fileName){
-    //system("top");
     char temp[256]={0};
-    strcpy(temp,"java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner qr.png");
-    //strcat(temp, fileName);
-    //strcat(temp,"\0");
-    //strcat(temp, " 2>out 1>out && echo \"hello\" >> out ");
-    //system(temp);
-    //cout<<"Return val: "<<system("touch /home/cs3516/Desktop/qr_forked_server/qrcodes/bin/Debug/newfile") << endl;
-    //return "foobar\n";
-
+    strcpy(temp,"java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner ");
+    strcat(temp, fileName);
     FILE* fp;
     fp = popen(temp,"r");
     char* line= (char*)malloc(1024 * sizeof(char));
     size_t len=1024;
-
     while((getline(&line,&len,fp)) > 0){
-        //cout<<"IN WHILE"<<endl;
-        //cout << line;
-
         if(strcmp(line,"Parsed result:\n")==0){
             getline(&line,&len,fp);
             pclose(fp);
             return line;
         }
     }
-
     pclose(fp);
     return "you dun goofed\n";
 }
@@ -117,10 +79,7 @@ void init(char* p){
     listen(sockfd,1); //listens to socket
 }
 
-int main(int argc, char** argv)
-{
-
-    cout << qr2url("qr.png");
+int main(int argc, char** argv){
     //config variables that hold the commandline args
     //-p 12345 -q 6 -r 30 -m 5 -t 60
     char* port="2012";//p
@@ -128,8 +87,6 @@ int main(int argc, char** argv)
     int rateTime=60;//r
     int maxUsers=3;//m
     int timeOut=80;//t
-
-    char* dir = get_current_dir_name();
 
     //do getopt funtime
     int c;
@@ -151,7 +108,7 @@ int main(int argc, char** argv)
             timeOut=atoi(optarg);
             break;
         case '?':
-            cout<< "you id something wrong" << endl;
+            cout<< "you did something wrong" << endl;
             break;
         }
 
@@ -163,13 +120,24 @@ int main(int argc, char** argv)
     //for the forking
     int status;
 
+    //open the log file
+    FILE* log=fopen("log.txt","a");
+    //get the time of day
+    time_t rawtime;
+    struct tm* timeinfo;
+    time(&rawtime);
+    timeinfo=localtime(&rawtime);
+    fprintf(log,"Log file started for %d-%d-%d %d:%d:%d \n",timeinfo->tm_year+1900,timeinfo->tm_mon+1,timeinfo->tm_mday,timeinfo->tm_hour,timeinfo->tm_min,timeinfo->tm_sec);
+    fflush(log);
+    //fclose(log);
+
     socklen_t sin_size;
     struct sockaddr_storage their_addr;
     char s[INET6_ADDRSTRLEN];
     char buf[65536]={0};
     char client_buf[65536]={0};
+    int numUsers=0;
 
-    //cout<<qr2url("qr.png", dir);
     cout<<"Listening..."<<endl;
     while(1){
         sin_size=sizeof their_addr;
@@ -180,10 +148,16 @@ int main(int argc, char** argv)
         if(new_fd==-1){
             continue;
         }
-
+        inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr*)&their_addr),s,sizeof s);
+        time(&rawtime);
+        timeinfo=localtime(&rawtime);
+        fprintf(log,"%d-%d-%d %d:%d:%d Connection from: %s\n",timeinfo->tm_year+1900,timeinfo->tm_mon+1,timeinfo->tm_mday,timeinfo->tm_hour,timeinfo->tm_min,timeinfo->tm_sec,s);
+        fflush(log);
+        numUsers++;
+        cout<< "num users " << numUsers << endl;
         pid_t pID = fork();
 
-        if(pID == 0){ //child
+        if(pID==0){ //child
             cout<<"This is child of pID: "<<pID<<endl;
             //Create a new file for output
             FILE *filet = fopen("client_qr.png", "w+");
@@ -199,14 +173,62 @@ int main(int argc, char** argv)
                         fputc(client_buf[i],filet);
                     }
                 }
+                else{
+                    continue;
+                }
                 fclose(filet);
                 break;
             }
             cout<<"Decoding QR"<<endl;
             //QR DECODING HERE
             inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr*)&their_addr),s,sizeof s);
-            cout<<qr2url("client_qr.png");
-            send(new_fd,qr2url("client_qr.png"),128,0);
+
+            int returnCode = 0;
+
+            char* url = qr2url("client_qr.png");
+
+            //GET RETURN CODE HERE
+            if ( &url[0] != "-" && &url[1] != "1"){
+                returnCode = 0; //0 = Success
+            }
+            else{
+                returnCode = 1;
+            }
+
+
+            char message2Client[65536] = {0};
+            char retCode = (char)(((int)'0')+returnCode);
+            char *newCode = &retCode;
+
+            strcpy( message2Client, newCode );
+
+            switch ( returnCode ){
+                case 0:
+                    strcat( message2Client, " - Success \n ");
+                    strcat( message2Client, url);
+                    break;
+                case 1:
+                    strcat( message2Client, " - Failure \n ");
+                    strcat( message2Client, "No URL or something went wrong.");
+                    break;
+                case 2:
+                    strcat( message2Client, " - Timeout \n ");
+                    break;
+                case 3:
+                    strcat( message2Client, " - Rate Limit Exceeded \n ");
+                    break;
+                default:
+                    break;
+            }
+
+            send(new_fd,message2Client,128,0);
+
+            //cout<<qr2url("client_qr.png");
+            //send(new_fd,qr2url("client_qr.png"),128,0);
+            time(&rawtime);
+            timeinfo=localtime(&rawtime);
+            fprintf(log,"%d-%d-%d %d:%d:%d Decoded a qr code with url: %s\n",timeinfo->tm_year+1900,timeinfo->tm_mon+1,timeinfo->tm_mday,timeinfo->tm_hour,timeinfo->tm_min,timeinfo->tm_sec,qr2url("client_qr.png"));
+            fflush(log);
             remove("client_qr.png");
             cout<<"Finished sending packet."<<endl;
             close(new_fd);
@@ -223,7 +245,9 @@ int main(int argc, char** argv)
             new_fd = -1;
             //listen(sockfd,1); //listens to socket
             //wait for child
-            //waitpid(pID,&status,WEXITED);
+
+            //while(!waitpid(pID,&status,WNOHANG)){}
+            //cout << "done waiting" << endl;
         }
     }
     return 0;
--- qrcodes_client/main.cpp
+++ qrcodes_client/main.cpp
@@ -16,6 +16,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdlib.h>
+#include <limits.h>
 
 using namespace std;
 
@@ -29,50 +30,66 @@ struct Packet
 
 }; // end of packet structure
 
+/* Returns the size of the packet
+    @param Packet pointer to a packet to be sent
+    @return int size
+*/
 int getPacketSize( Packet* packet)
 {
     return sizeof (packet->size) + sizeof (packet->data);
 }
 
+/*
+    Creates and returns a packet of a file.
+    @param filePath - path to the file
+    @return Packet struct
+*/
 Packet createPacket(char* filePath){
 
     //Packet creation:
-
     Packet packet = {0};
 
     FILE *file;
     file = fopen(filePath, "r");
-    cout<<"FILEPATH: "<<filePath<<endl;
+    cout<<"FILE PATH: "<<filePath<<endl;
 
-    //packet.size = getFileSize (file);
     struct stat fStat;
     stat (filePath, &fStat);
     packet.size = fStat.st_size;
+    if (packet.size > 65536 || packet.size <= 0){
+        cerr<<"File size invalid \n";
+        exit(1);
+    }
 
     cout<<"Size of packet: "<<packet.size<<endl;
     packet.data = malloc( packet.size );
-    if ( packet.data == 0)
-        cout<<"ERROR: FILE EMPTY"<<endl;
-
-    cout<<"Size of packet data: "<<packet.data<<endl;
-    int n = fread(packet.data, packet.size, 1, file);
-    cout<<"Whatever n is: "<<n<<endl;
+    if ( packet.data == 0){
+        cerr<<"ERROR: FILE EMPTY \n"<<endl;
+        exit(1);
+    }
+    fread(packet.data, packet.size, 1, file);
     return packet;
 }
 
-//returns -1 on failure, bytesSent on success
+/*
+    returns -1 on failure, bytes sent on success
+    @param sockfd - int socket
+    @param buffer - point to buffer where the data is stored
+    @bytesLeft - amount of bytes to send
+*/
 int sendInChunks(int sockfd, char *buffer, int bytesLeft)
 {
     int bytesSent = 0;        // how many bytes we've sent
     int bytesSentSoFar = 0;
 
-    cout<<"socket/bytesLeft "<< sockfd << " / " << bytesLeft << endl;
+    cout<<"Bytes Left " << bytesLeft << endl;
 
     while(bytesLeft > 0) {
                             //socket, buffer iterator,  bytes left, flags
         bytesSentSoFar = send(sockfd, buffer+bytesSent, bytesLeft, 0);
         if (bytesSentSoFar == -1 ){
-            cout<<"ERROR 001: -1 bytes sent."<<endl;
+            cerr<<"ERROR: -1 bytes sent. \n"<<endl;
+            exit(1);
             break;
         }
         bytesSent += bytesSentSoFar;
@@ -80,8 +97,8 @@ int sendInChunks(int sockfd, char *buffer, int bytesLeft)
     }
 
     if (bytesSentSoFar == -1 ){
-        cout<<"ERROR 002: -1 bytes were sent."<<endl;
-        return -1;
+        cerr<<"ERROR: -1 bytes were sent. \n"<<endl;
+        exit(1);
     }
     else {
         return bytesSent;
@@ -89,9 +106,8 @@ int sendInChunks(int sockfd, char *buffer, int bytesLeft)
 }
 
 
-///////////// END OF PACKET
+///////////// END OF PACKET PROGRAMMING - LZ
 
-//for the checkpoint, the client will cin to a string and then the server will print that string
 
 //helper function to determine if we are using ipv4 or 6
 void* get_in_addr(struct sockaddr* sa){
@@ -103,26 +119,44 @@ void* get_in_addr(struct sockaddr* sa){
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main()
+int main(int argc, char** argv)
 {
-    //connect to server on 12345
-    //listen for messages from the server
-    //then print them
-    int sockfd, numbytes, new_fd;
+    int sockfd, numbytes;
     char buf[256];
-    struct addrinfo hints, *serverinfo, *p;
+    struct addrinfo hints, *serverinfo;
     struct their_addr;
-        socklen_t sin_size;
 
     int rv;
     char s[INET6_ADDRSTRLEN];
 
-    memset(&hints,0,sizeof hints);
+    memset(&hints,0,sizeof hints); //set memory to safe 0
     hints.ai_family=AF_UNSPEC;
     hints.ai_socktype=SOCK_STREAM;
 
+
+    char* port = "2012"; //default port
+    char* IP = "127.0.0.1"; //default IP
+
+    //GETOPT
+    //    p: -> -p  [something]
+    //    i: -> -i [something_else]
+    int c;
+    while((c=getopt(argc,argv,"p:i:"))!=-1){
+        switch(c){
+        case 'p':  //port
+            port=optarg;
+            break;
+        case 'i':
+            IP=optarg;
+            break;
+        case '?':
+            cout<< "you id something wrong" << endl;
+            break;
+        }
+    }
+
     //connection setting
-    rv=getaddrinfo("127.0.0.1","12345",&hints,&serverinfo);
+    rv=getaddrinfo(IP, port,&hints,&serverinfo);
 
     //creation of socket
     sockfd=socket(serverinfo->ai_family,serverinfo->ai_socktype,serverinfo->ai_protocol);
@@ -132,29 +166,26 @@ int main()
     cout << "connecting to: "<< s << endl;
 
     bool b_msg_sent = false;
-    //msg buffer
-    char str_msg_buf[256];
     char filePath[256];
 
     while(1){
 
         if(!b_msg_sent){
-            cout<<"Give a file path"<<endl;
+
+            cout << "Give a file path"<<endl;
             cin>>filePath;
 
             Packet p = createPacket(filePath);
-            cout << "Sending packet"<< endl;
+            cout << "Sending packet "<< endl;
 
             int foo = sendInChunks( sockfd, (char *)p.data, p.size);
-            cout<<"sent bits"<<foo<<endl;
-            //send(sockfd,filePath,128,0);
-            free(p.data);
-            //send message
-            //cout<<"Attempting to send message: "<<str_msg_buf<<endl;
-            cout<<"Sending to "<< s <<endl;
+            cout<<"Sent bytes "<<foo<<endl;
 
-            //int foo = send(sockfd,str_msg_buf,128,0);
-            //cout<<"sent bits"<<foo<<endl;
+            //FREE MEMORY
+            free(p.data);
+
+            //send message
+            cout<<"Sending to "<< s <<endl;
 
             b_msg_sent = true;
             cout<<"Awaiting response..."<<endl;
@@ -173,7 +204,6 @@ int main()
             }
         }
     }
-
 
     return 0;
 }
